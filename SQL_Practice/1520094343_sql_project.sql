5c4e7d7bd9cff3917d14b5a9fad546c6022d6214
Harshinder Chadha <40350555+hershcode@users.noreply.github.com>
2018-09-12 23:04:43 -0700

    Update 1520094343_sql_project.sql
    - First 5 questions have correct queries
    - Still need correct code for 6 and 7

--- SQL_Practice/1520094343_sql_project.sql
+++ SQL_Practice/1520094343_sql_project.sql
@@ -26,36 +26,65 @@ exploring the data, and getting acquainted with the 3 tables. */
 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
-
+SELECT * 
+FROM  `Facilities` 
+WHERE membercost >0
 
 /* Q2: How many facilities do not charge a fee to members? */
-
+SELECT *, 
+	   COUNT(facid) AS no_charge_facilities_count
+FROM  `Facilities` 
+WHERE membercost = 0
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-
+SELECT facid,
+       name,
+       membercost,
+       monthlymaintenance
+FROM `Facilities`
+WHERE membercost < (0.2 * monthlymaintenance)
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
-
+SELECT *
+FROM `Facilities`
+WHERE facid IN (1,5)
 
 /* Q5: How can you produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100? Return the name and monthly maintenance of the facilities
 in question. */
-
+SELECT name,
+       monthlymaintenance,
+       CASE WHEN monthlymaintenance > 100 THEN 'expensive'
+       ELSE 'cheap' END AS cheap_or_expensive
+FROM `Facilities`
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Do not use the LIMIT clause for your solution. */
-
+SELECT firstname,
+       surname,
+       joindate
+FROM `Members`
+HAVING joindate = MAX(joindate)
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
-
+SELECT m.firstname,
+       m.surname,
+       f.name
+FROM `Bookings` b
+INNER JOIN `Facilities` f
+  ON b.facid = f.facid
+INNER JOIN `Members` m
+  ON b.memid = m.memid
+WHERE f.name Like '%Tennis Court%'
+ORDER BY m.firstname
 
 /* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30? Remember that guests have
